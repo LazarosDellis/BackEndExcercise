603af39eb97537e7dc271a3edc4e848f11146da4
Larry <Larry@DESKTOP-1MTPNFU>
2021-01-20 13:52:00 +0200

    JavaSpringBoot

--- registration/src/main/java/com/app/registration/RegistrationApplication.java
+++ registration/src/main/java/com/app/registration/RegistrationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegistrationApplication {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationApplication.class, args);
 	}
